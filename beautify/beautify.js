95ef6c4a52ea85df6c9184fb826d1fa34c6c70b5
SparkleTeamOfficial <50514290+SparkleTeamOfficial@users.noreply.github.com>
2019-05-11 17:53:58 +0200

    Create beautify.js

---
+++ beautify/beautify.js
@@ -0,0 +1,17 @@
+module.exports.run = async (bot, message, args) => {
+    message.delete();
+    let lang = args[0].toLowerCase();
+    let x = args.slice(1).join(" ");
+    let y = `\`\`\`${lang}\n${beautify(x, {format: "js"})}\n\`\`\``;
+    message.channel.send(y);
+}
+
+module.exports.command = {
+name: 'beautify',
+aliases: ["btfy"],
+permission: "",
+description: "Beautify code.",
+usage: ">beautify CODE!",
+category: "Owner",
+enabled: true
+};
